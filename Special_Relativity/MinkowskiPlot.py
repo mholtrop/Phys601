9f21f6cfe1a0c22a6c837cf85133eba37d008f70
Maurik Holtrop <maurik@physics.unh.edu>
2020-09-11 10:14:37 -0400

    Add color processing so grid lines can have alpha<1

--- Special_Relativity/MinkowskiPlot.py
+++ Special_Relativity/MinkowskiPlot.py
@@ -6,6 +6,8 @@ import math as m
 import numpy as np
 import plotly.graph_objects as go
 import plotly.io as pio
+from matplotlib import colors as ccc
+
 pio.templates.default="plotly_white"
 
 
@@ -31,7 +33,7 @@ class Actor(object):
         self.name = name
         self.velocity = velocity
         self.position = position
-        self.color = color
+        self.color = self.convert_color(color)
         self.size = size
 
         self.NDots = 20
@@ -40,6 +42,17 @@ class Actor(object):
 
         self.gamma = 1/(m.sqrt(1-velocity*velocity))
 
+    def convert_color(self, color):
+        """Take in a color name and convert it to a plotly color tuple."""
+        color_tuple = None
+        if type(color) is str:
+            ct = ccc.to_rgb(color)
+            return (int(ct[0]*255), int(ct[1]*255), int(ct[2]*255))
+        elif type(color) is tuple or type(color) is list:
+            return color
+
+    def color_rgba(self, alpha=1.):
+        return "rgba({:d},{:d},{:d},{:4.3f})".format(self.color[0], self.color[1], self.color[2], alpha)
 
 class MinkowsiPlot(object):
 
@@ -48,7 +61,7 @@ class MinkowsiPlot(object):
         self._actors = []
         self._fig = None
         self.SliderSteps = 21
-        self.ShowAltAxes = True
+        self.ShowAltAxes = False
         self._xmin = -10
         self._xmax = 10
         self._ymin = -10
@@ -79,6 +92,8 @@ class MinkowsiPlot(object):
                 width=width,
                 height=height,
                 title="Minkowski Diagram",
+                xaxis_title="x [light-seconds]",
+                yaxis_title="t [seconds]"
             ))
 
         for actor in self._actors:
@@ -121,44 +136,69 @@ class MinkowsiPlot(object):
                     yy.append(t_prime(t, actor.position + actor.velocity*t, u))
 
             fig.add_trace(go.Scatter(
-                marker=dict(color=actor.color, size=actor.size),
-                line=dict(color=actor.color, width=0.3),
+                marker=dict(color=actor.color_rgba(1.), size=actor.size),
+                line=dict(color=actor.color_rgba(0.8), width=1.0),
                 mode='lines+markers',
                 x=xx,  # [ x_prime(t,0.*t,u) for t in range(10)],
                 y=yy,  # [ t_prime(t,0.*t,u) for t in range(10)],
                 visible=False,
-                name="{} at u={:3.1f}".format(actor.name,u),
+                name="{}".format(actor.name),
+                # name="{} at u={:3.1f}".format(actor.name,u),
             ))
 
-            angle_x = m.atan(rel_add_velocity(actor.velocity,u))
-            (x_grid, y_grid) = self.make_grid(angle_x, angle_x)
+            (x_grid, y_grid) = self.make_grid(rel_add_velocity(u, -actor.velocity))
             fig.add_trace(go.Scatter(
                 # marker=dict(color=actor.color, size=actor.size),
-                line=dict(color=actor.color, width=0.1),
-                mode='lines+markers',
+                line=dict(color=actor.color_rgba(0.3), width=0.5),
+                mode='lines',
                 x=x_grid,
                 y=y_grid,
                 visible=False,
-                name="A: for {} at u={:3.1f}".format(actor.name,u),
+                showlegend=False,
+                # name="A: for {} at u={:3.1f}".format(actor.name, u),
 
             ))
 
-    def make_grid(self, angle_x, angle_y):
+    def make_grid(self, beta):
+
         NGridSteps = 21 * 2
         Zmin = -10 * 2
         Zmax = 10 * 2
-        x0_min = Zmin * m.tan(angle_y)
-        x0_max = Zmax * m.tan(angle_y)
-        y0_min = Zmin * m.tan(angle_x)
-        y0_max = Zmax * m.tan(angle_x)
 
-        yy1 = [Zmin, Zmax, None] * NGridSteps
-        xx1 = list(np.array([(x - (3 * NGridSteps // 2 - 1)) // 3 for x in range(3 * NGridSteps)]) +
-                   np.array([x0_min, x0_max, 0] * NGridSteps))
+        # Draw the vertical lines.
+        xx1=[]
+        yy1=[]
+        for i in range(NGridSteps):
+            x = Zmin + i
+            t_up = Zmax
+            t_down = Zmin
+            xx1.append(x_prime(t_down, x, beta))
+            yy1.append(t_prime(t_down, x, beta))
+            xx1.append(x_prime(t_up, x, beta))
+            yy1.append(t_prime(t_up, x, beta))
+            xx1.append(None)
+            yy1.append(None)
+        #
+        # yy1 = [Zmin, Zmax, None] * NGridSteps
+        # xx1 = list(np.array([(x - (3 * NGridSteps // 2 - 1)) // 3 for x in range(3 * NGridSteps)]) +
+        #            np.array([x0_min, x0_max, 0] * NGridSteps))
 
-        xx2 = [Zmin, Zmax, None] * NGridSteps
-        yy2 = list(np.array([(x - (3 * NGridSteps // 2 - 1)) // 3 for x in range(3 * NGridSteps)]) +
-                   np.array([y0_min, y0_max, 0] * NGridSteps))
+        xx2 = []
+        yy2 = []
+        for i in range(NGridSteps):
+            t = Zmin + i
+            x_left = Zmax
+            x_right = Zmin
+            xx2.append(x_prime(t, x_left, beta))
+            yy2.append(t_prime(t, x_left, beta))
+            xx2.append(x_prime(t, x_right, beta))
+            yy2.append(t_prime(t, x_right, beta))
+            xx2.append(None)
+            yy2.append(None)
+
+        # xx2 = [Zmin, Zmax, None] * NGridSteps
+        # yy2 = list(np.array([(x - (3 * NGridSteps // 2 - 1)) // 3 for x in range(3 * NGridSteps)]) +
+        #            np.array([y0_min, y0_max, 0] * NGridSteps))
 
         x_grid = xx1 + xx2
         y_grid = yy1 + yy2
@@ -177,12 +217,12 @@ class MinkowsiPlot(object):
                 args=[{"visible": [False] * (2*NSets * self.SliderSteps + 1)},
                       {"title": "Minkowski Space Time with boost u = {:4.2f} c".format(u)},
                       ],  # layout attribute
-                label=int(u * 10) / 10.,
+                label="{:3.1f}".format(u),
             )
 
             for j in range(NSets):
                 step["args"][0]["visible"][2*self.SliderSteps * j + 2*i] = True  # Toggle i'th trace to "visible"
-                step["args"][0]["visible"][2*self.SliderSteps * j + 2*i+1] = False  # Toggle i'th trace to "visible"
+                step["args"][0]["visible"][2*self.SliderSteps * j + 2*i+1] = self.ShowAltAxes  # Toggle i'th trace to "visible"
 
             step["args"][0]["visible"][2*NSets * self.SliderSteps] = True  # Toggle last trace to "visible"
             steps.append(step)
@@ -197,7 +237,7 @@ class MinkowsiPlot(object):
 
         for j in range(NSets):
             self._fig.data[2*self.SliderSteps * j + 2 * (self.SliderSteps // 2)].visible = True
-            self._fig.data[2*self.SliderSteps * j + 2 * (self.SliderSteps // 2) + 1].visible = False
+            self._fig.data[2*self.SliderSteps * j + 2 * (self.SliderSteps // 2) + 1].visible = self.ShowAltAxes
 
         # if len(self._fig.data) > NSets*NSets:
         #     for j in range(NSets*NSets, len(self._fig.data)):
